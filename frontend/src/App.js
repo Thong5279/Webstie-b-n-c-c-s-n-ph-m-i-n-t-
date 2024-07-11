08d9abc4d8dcdff96ff9915568c61d87334f761d
PhamHuynhThong <numuanuocnoi@gmail.com>
2024-07-11 23:46:30 +0700

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+function App() {
+  return (
+   <>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+   </>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import Home from "../pages/Home"
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <App/>,
+        children  : [
+            {
+                path : "",
+                element : <Home/>
+            }
+        ]
+    }
+])
+export default router
