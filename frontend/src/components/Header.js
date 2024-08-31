85a7d78d46e7174038b238b5cf5ebdb910b71b5c
PhamHuynhThong <numuanuocnoi@gmail.com>
2024-08-31 22:42:39 +0700

    Phân quyền thành công

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -60,7 +60,10 @@ const Header = () => {
         <div className='flex items-center gap-7'>
 
            <div className='relative flex justify-center'>
-            <div className='text-3xl cursor-pointer relative flex justify-center'  onClick={()=>setMenuDisplay(preve => !preve)}>
+
+            {
+              user?._id &&(
+                <div className='text-3xl cursor-pointer relative flex justify-center'  onClick={()=>setMenuDisplay(preve => !preve)}>
                   {
                     user?.profilePic ?(
                       <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name}/>//them hinh anh 
@@ -71,12 +74,20 @@ const Header = () => {
                   }
             
               </div>
+              )
+            }
+            
               
               {
                 menuDisplay &&(
                     <div className='absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded'>
                     <nav>
-                      <Link to={"admin-panel"} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={()=>setMenuDisplay(preve => !preve)}>Admin Panel</Link>
+                      {
+                        user?.role === ROLE.ADMIN && (
+                          <Link to={"/admin-panel/all-product"} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={()=>setMenuDisplay(preve => !preve)}>Admin Panel</Link>
+                        )
+                      }
+                     
                     </nav>
                   </div>
                 )
--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { FaCircleUser, FaRegCircleUser } from "react-icons/fa6";
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import ROLE from '../common/role';
 const AdminPanel = () => {
     const user = useSelector(state => state?.user?.user)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(user?.role !== ROLE.ADMIN){
+            navigate("/")
+        }
+    },[])
     
   return (
     <div className='min-h-[calc(100vh-120px)] md:flex hidden'>
