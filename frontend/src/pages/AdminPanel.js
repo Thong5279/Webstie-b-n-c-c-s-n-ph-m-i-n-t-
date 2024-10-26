15b738130cefc3e33f1eed0ebeea5f04b2e20be6
PhamHuynhThong <numuanuocnoi@gmail.com>
2024-10-26 22:28:46 +0700

    update icon Product low stock

--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -3,6 +3,10 @@ import { useSelector } from 'react-redux'
 import { FaCircleUser, FaRegCircleUser } from "react-icons/fa6";
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import ROLE from '../common/role';
+import { FaUsers } from "react-icons/fa";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { RiAlertFill } from "react-icons/ri";
+
 const AdminPanel = () => {
     const user = useSelector(state => state?.user?.user)
     const navigate = useNavigate()
@@ -34,8 +38,18 @@ const AdminPanel = () => {
             {/*Điều hướng  */}
             <div>
                     <nav className='grid'>
-                        <Link to={"all-user"} className='px-2 py-1 hover:bg-slate-100'>Người dùng trên hệ thống</Link>
-                        <Link to={"all-product"} className='px-2 py-1 hover:bg-slate-100'>Sản phẩm Trên hệ thống</Link>
+                        <Link to={"all-user"} className='px-2 py-1 hover:bg-slate-100 flex items-center'>
+                            <FaUsers className="mr-2 text-2xl text-blue-600" />
+                            Người dùng trên hệ thống
+                        </Link>
+                        <Link to={"all-product"} className='px-2 py-1 hover:bg-slate-100 flex items-center'>
+                            <MdProductionQuantityLimits className="mr-2 text-2xl text-green-600" />
+                            Sản phẩm Trên hệ thống
+                        </Link>
+                        <Link to={"low-stock-products"} className='px-2 py-1 hover:bg-slate-100 flex items-center'>
+                            <RiAlertFill className="mr-2 text-2xl text-red-600" />
+                            Sản phẩm sắp hết hàng
+                        </Link>
                     </nav>
             </div>
 
