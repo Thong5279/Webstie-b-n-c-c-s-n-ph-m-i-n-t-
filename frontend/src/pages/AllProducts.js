ef50b1ff94d1081919d090e24dc8300fd72f7482
NguyenDThai <thainguyen4646@gmail.com>
2024-10-28 08:46:01 +0700

    checkbox và các nút thanh toán

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -25,9 +25,8 @@ function AllProducts() {
     <div>
       <div className='bg-white p-2 px-4 flex justify-between items-center'>
         <h2 className='font-bold text-lg'>Sản Phẩm</h2>
-        <button className='border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-2 px-4 rounded-full' onClick={()=>setOpenUploadProduct(true)}>Upload Sản phẩm</button>
-      </div>
-
+          <button className='border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-2 px-4 rounded-full' onClick={()=>setOpenUploadProduct(true)}>Upload Sản phẩm</button>
+        </div>
     {/*all product*/}
       <div className='flex flex-wrap gap-5 py-4 h-[calc(100vh-190px)] overflow-y-scroll justify-center'>
           {
--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -3,6 +3,8 @@ import SummaryApi from '../common'
 import Context from '../context'
 import displayVNDCurrency from '../helpers/displayCurrency'
 import { FaTrash } from "react-icons/fa";
+import { FaPaypal } from "react-icons/fa6";
+import { ImQrcode } from "react-icons/im";
 
 const Cart = () => {
     const [data,setData] = useState([])
@@ -133,26 +135,29 @@ const Cart = () => {
                         ) : (
                            data.map((product,index)=>{
                             return(
-                                <div key={product?._id+"Add to cart loading"} className='w-full bg-white h-32 my-2 border-slate-300 rounded grid grid-cols-[128px,1fr]'>
-                                    <div className='w-32 h-32 bg-slate-200'>
-                                        <img src={product?.productId?.productImage[0]} />
-                                    </div>
-                                    <div className='px-4 py-2 relative'>
-                                        {/* delete product */}
-                                        <div className='absolute right-0 text-red-600 rounded-full p-2 hover:bg-red-600 hover:text-white cursor-pointer' onClick={()=>deleteProduct(product?._id)}>
-                                             <FaTrash />
+                                <div className='flex'>
+                                    <input type='checkbox' className='w-5 mx-4 bg-red-500 cursor-pointer'/>
+                                    <div key={product?._id+"Add to cart loading"} className='w-full bg-white h-32 my-2 border-slate-300 rounded grid grid-cols-[128px,1fr]'>
+                                        <div className='w-32 h-full bg-slate-200'>
+                                            <img src={product?.productId?.productImage[0]} className='h-full object-cover' />
                                         </div>
-                                        <h2 className='text-lg lg:text-2xl text-ellipsis line-clamp-1'>{product?.productId?.productName}</h2>
-                                        <p className='capitalize text-slate-500'>{product?.productId?.category}</p>
-                                        <div className='flex items-center justify-between'>
-                                            <p  className='text-red-600 font-medium text-lg'>{displayVNDCurrency(product?.productId?.sellingPrice)}</p>
-                                            <p  className='text-slate-500 font-medium text-lg line-through'>{displayVNDCurrency(product?.productId?.price)}</p>
-                                            <p  className='text-slate-600 font-semibold text-lg'>{displayVNDCurrency(product?.productId?.sellingPrice * product?.quantityCart)}</p>
-                                        </div>
-                                        <div className='flex items-center gap-3 mt-2'>
-                                            <button className=' border border-red-600 text-red-600 rounded-full hover:bg-red-600 hover:text-white w-6 h-6 flex items-center justify-center' onClick={()=>decreaseQty(product?._id,product?.quantityCart)}>-</button>
-                                            <span>{product?.quantityCart}</span>
-                                            <button className=' border border-red-500 rounded-full hover:bg-red-500 hover:text-white w-6 h-6 flex items-center justify-center' onClick={()=>increaseQty(product?._id,product?.quantityCart)}>+</button>
+                                        <div className='px-4 relative'>
+                                            {/* delete product */}
+                                            <div className='absolute right-0 text-red-600 rounded-full p-2 hover:bg-red-600 hover:text-white cursor-pointer' onClick={()=>deleteProduct(product?._id)}>
+                                                <FaTrash />
+                                            </div>
+                                            <h2 className='text-lg lg:text-2xl text-ellipsis line-clamp-1'>{product?.productId?.productName}</h2>
+                                            <p className='capitalize text-slate-500'>{product?.productId?.category}</p>
+                                            <div className='flex items-center justify-between'>
+                                                <p  className='text-red-600 font-medium text-lg'>{displayVNDCurrency(product?.productId?.sellingPrice)}</p>
+                                                <p  className='text-slate-500 font-medium text-lg line-through'>{displayVNDCurrency(product?.productId?.price)}</p>
+                                                <p  className='text-slate-600 font-semibold text-lg'>{displayVNDCurrency(product?.productId?.sellingPrice * product?.quantityCart)}</p>
+                                            </div>
+                                            <div className='flex items-center gap-3 mt-2'>
+                                                <button className=' border border-red-600 text-red-600 rounded-full hover:bg-red-600 hover:text-white w-6 h-6 flex items-center justify-center' onClick={()=>decreaseQty(product?._id,product?.quantityCart)}>-</button>
+                                                <span>{product?.quantityCart}</span>
+                                                <button className=' border border-red-500 rounded-full hover:bg-red-500 hover:text-white w-6 h-6 flex items-center justify-center' onClick={()=>increaseQty(product?._id,product?.quantityCart)}>+</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -164,26 +169,43 @@ const Cart = () => {
 
 
                 {/*sumary product*/}
-            <div className='mt-5 lg:mt-0 w-full max-w-sm'>
+            <div className='mt-5 lg:mt-0 w-full max-w-full'>
             {
                     loading ? (
                         <div className='h-36 bg-slate-200 border border-slate-300 animate-pulse '>
                         
                         </div>
                     ) : (
-                        <div className='h-36 bg-white'>
+                        <div className='min-h-36 bg-white'>
                             <h2 className='text-white bg-red-600 px-4 py-1'>Tổng Đơn Hàng</h2>
-                            <div className='flex items-center justify-between px-4 font-medium gap-2 text-lg text-slate-600'>
+                            <div className='flex items-center justify-between px-4 py-4 font-medium gap-2 text-lg text-slate-600'>
                                 <p>Số Lượng:</p>
                                 <p>{totalQty}</p>
                             </div>
-                            <div className='flex items-center justify-between px-4 font-medium gap-2 text-lg text-slate-600'>
+                            <div className='flex items-center justify-between px-4 py-4 font-medium gap-2 text-lg text-slate-600'>
                                 <p>Địa chỉ nhận hàng:</p>
                                 <p>{context.user?.address}</p>
                             </div>
-                            <div className='flex items-center justify-between px-4 font-medium gap-2 text-lg text-slate-600'>
+                            <div className='flex items-center justify-between px-4 py-4 font-medium gap-2 text-lg text-slate-600'>
                                 <p>Tổng Cộng:</p>
                                 <p>{displayVNDCurrency(totalPrice)}</p>
+                            </div>
+                                {/* Ma giam gia */}
+                            <div className='px-4 py-4 font-medium text-slate-600'>
+                                <p className='pb-2 text-left'>Mã Giảm Giá:</p>
+                                <input placeholder='Nhập mã giảm giá' className='outline-none border border-solid p-3 w-full' />
+                            </div>
+
+                            <div className='px-4 py-4 font-medium text-slate-600 flex flex-col gap-6'>
+                                <p>Phương Thức Thanh Toán:</p>
+                                <button className='py-2 px-6 bg-[#0070ba] text-white flex items-center justify-center rounded-full hover:bg-white hover:text-[#0070ba] hover:border hover:border-[#0070ba]'>
+                                    <FaPaypal className='mr-2'/>
+                                    PayPal
+                                    </button>
+                                <button className='flex items-center justify-center py-2 px-6 bg-red-500 text-white rounded-full hover:bg-white hover:text-red-600 hover:border hover:border-red-600'>
+                                    <ImQrcode className='mr-2'/>
+                                    QR Pay
+                                    </button>
                             </div>
 
                             <button className='bg-red-600 w-full text-white p-2 mt-4 hover:bg-white hover:text-red-600'>Thanh Toán</button>
